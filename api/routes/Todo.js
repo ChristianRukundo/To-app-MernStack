225a98d78af192b53845a2ee004783725e17a194
ChristianRukundo <siboruremarukundoc1@gmail.com>
2023-06-22 09:20:37 +0200

    created routes

--- api/routes/Todo.js
+++ api/routes/Todo.js
@@ -6,6 +6,7 @@ const User = require("../models/User");
 
 router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
+  
   try {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
